762fcad29ffe42ae36077f273a9b8ea4965bb3d1
Art Williams <art@fearthewild.com>
2019-04-28 19:21:54 +0100

    no message

--- inc/config/db.php
+++ inc/config/db.php
@@ -2,10 +2,10 @@
   // DB Params
   define("DB_HOST", "localhost");
   define("DB_USER", "root");
-  define("DB_PASS", "Usacaladpw1991!");
+  define("DB_PASS", "pass");
   define("DB_NAME", "designership");
 
   function getConn() {
-    return new PDO("mysql:host=localhost;dbname=designership", 'root', 'Usacaladpw1991!');
+    return new PDO("mysql:host=localhost;dbname=designership", 'root', 'pass');
   }
   $dbh = getConn();
